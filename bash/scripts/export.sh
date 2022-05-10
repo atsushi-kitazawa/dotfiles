ffb19bfddd57b170a05d17fadf421b81682b5a72
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-05-10 14:20:36 +0900

    update export.sh

--- bash/scripts/export.sh
+++ bash/scripts/export.sh
@@ -2,8 +2,8 @@
 
 # set JAVA_HOME
 #export JAVA_HOME=/usr/local/opt/openjdk@11
-export JAVA_HOME=/Users/atsushi/Runtime/java/jdk-11.0.9+11/Contents/Home/
-export JMC_HOME=/Users/atsushi/devtool/java/jmc-8.0.1_osx-x64/JDK\ Mission\ Control.app/Contents/MacOS/
+export JAVA_HOME=$HOME/Runtime/java/jdk-11.0.9+11/Contents/Home/
+export JMC_HOME=$HOME/devtool/java/jmc-8.0.1_osx-x64/JDK\ Mission\ Control.app/Contents/MacOS/
 
 # set GOPATH
 #export GOPATH=$HOME/.go
@@ -13,10 +13,10 @@ export GOPATH=$HOME/workspace/golang
 export NODEBREW_HOME=$HOME/.nodebrew/node/v14.15.1
 
 # set BUILD TOOL PATH
-export ANT_HOME=/Users/atsushi/workspace/java/ant/apache-ant-1.10.9
+export ANT_HOME=$HOME/workspace/java/ant/apache-ant-1.10.9
 
 # Gitlab
-export GITLAB_HOME=/Users/atsushi/workspace/gitlab
+export GITLAB_HOME=$HOME/workspace/gitlab
 
 # set PATH
 export PATH=$PATH:$JAVA_HOME/bin:$JMC_HOME:$ANT_HOME/bin:$NODEBREW_HOME/bin
